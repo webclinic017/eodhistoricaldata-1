ceb089b1fdfe9c77ae2f2479da2070b5828af545
Michael Whittle <michael@lifecycle-ps.com>
2021-11-23 22:33:15 +0000

    added requests library to dependancies

--- eodhistoricaldata/__init__.py
+++ eodhistoricaldata/__init__.py
@@ -4,4 +4,4 @@ from eodhistoricaldata.apiclient import APIClient
 from eodhistoricaldata.websocketclient import WebSocketClient
 
 # Version of eodhistoricaldata package
-__version__ = "0.4.1"
+__version__ = "0.4.2"
